da15916f1d84b4f6085a68e3ea50ac47cb4f6b35
Marcio Nitao <marcionitao@gmail.com>
2020-12-26 17:15:40 +0100

    add lib vue-bootstrap and vue-codemirror

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue';
+import { BootstrapVue } from 'bootstrap-vue';
+import VueCodemirror from 'vue-codemirror';
 import App from './App.vue';
 import router from './router';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript';
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(VueCodemirror);
 
 new Vue({
   router,
