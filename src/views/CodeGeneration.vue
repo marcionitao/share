bd2659a5d8be6d658b7bdcf8051250d30469c4c1
Marcio Nitao <marcionitao@gmail.com>
2020-12-27 19:40:43 +0100

    add style and code format in page

--- src/views/CodeGeneration.vue
+++ src/views/CodeGeneration.vue
@@ -1,6 +1,52 @@
 <template>
-  <div class="home">
+  <b-container class="mt-4 ml-4">
+    <h1 class="mb-4">Code Share Plattform</h1>
 
-  </div>
+    <!--Adicionando o 'codemirror' e suas configurações a esta pagina-->
+    <codemirror v-model="code" :options='cmOptions' />
+
+    <b-button variant="info" class="mt-2" @click="createCode" :disabled='created'>
+      Create Code
+    </b-button>
+
+  </b-container>
 </template>
-<!-- Minuto  4:50 -->
+
+<script>
+export default {
+  data() {
+    return {
+      created: false,
+      code: 'const a = 10',
+    };
+  },
+
+  mounted() {
+    const urlCode = this.$route.query.code;
+    if (urlCode) {
+      this.code = urlCode;
+      this.created = true;
+    }
+  },
+
+  computed: {
+    cmOptions() {
+      return {
+        tabSize: 2,
+        mode: 'text/javascript',
+        lineNumbers: true,
+        readOnly: this.created ? 'nocursor' : false,
+      };
+    },
+  },
+  methods: {
+    createCode() {
+      this.created = true;
+      this.$router.push({ path: '', query: { code: this.code } });
+      this.code = '';
+      this.created = false;
+    },
+  },
+
+};
+</script>
